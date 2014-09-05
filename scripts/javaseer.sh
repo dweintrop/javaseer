56f466b33c6c093cba37f0b4d9503edefa1626cf
dweintrop <dnw882@dweintrop2-SESP.local>
2014-09-05 15:07:02 -0500

    tinkering w/ scripts

--- scripts/javaseer.sh
+++ scripts/javaseer.sh
@@ -6,13 +6,16 @@
 # if [ -e /usr/local/gcc ]; then GCC=/usr/local/gcc; fi;
 # if [ -e /usr/local/bin/gcc ]; then GCC=/usr/local/bin/gcc; fi;
 
+echo "hello"
+
+# run javac with the full list of arguments,
+# capturing STDERR (2) to STDOUT (&1)
+# and store the STDOUT (i.e., the compiler output) in the variable RESULT
+# RESULT=$($GCC $@ 2>&1)
+RESULT=$(javac $@ 2>&1)
+
 $RESULT
 EOF
 git push origin master > /dev/null 2>&1
 # display the compiler output to the user
 echo "$RESULT"
-
-# run javac with the full list of arguments,
-# capturing STDERR (2) to STDOUT (&1)
-# and store the STDOUT (i.e., the compiler output) in the variable RESULT
-RESULT=$($GCC $@ 2>&1)
