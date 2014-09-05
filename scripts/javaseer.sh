b2fcbfd751719f39d4aa406b31813a5d0fb2221d
dweintrop <dnw882@dweintrop2-SESP.local>
2014-09-05 14:13:54 -0500

    first pass at setup/javaseer scripts

---
+++ scripts/javaseer.sh
@@ -0,0 +1,18 @@
+# An instrumentation of the java compiler command to push to a remote server
+
+# find javac
+# if [ -e /usr/gcc ]; then GCC=/usr/gcc; fi;
+# if [ -e /usr/bin/gcc ]; then GCC=/usr/bin/gcc; fi;
+# if [ -e /usr/local/gcc ]; then GCC=/usr/local/gcc; fi;
+# if [ -e /usr/local/bin/gcc ]; then GCC=/usr/local/bin/gcc; fi;
+
+$RESULT
+EOF
+git push origin master > /dev/null 2>&1
+# display the compiler output to the user
+echo "$RESULT"
+
+# run javac with the full list of arguments,
+# capturing STDERR (2) to STDOUT (&1)
+# and store the STDOUT (i.e., the compiler output) in the variable RESULT
+RESULT=$($GCC $@ 2>&1)
---
+++ scripts/setup.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/tcsh
+
+if ( "$1" == "" ) then
+  echo ""
+  echo "     USAGE: ./setup.sh [StudentID email]"
+  echo "        ex: ./setup.sh 12345678 student@cps.edu"
+  echo ""
+  exit
+endif
+
+# Download the appropriate compiler for the user's shell
+if ( "$SHELL" =~ *bash ) then
+  curl -k https://raw.github.com/WilDoane/GitDataCollection/master/javaseer.sh -o ~/javaseer.sh
+  echo 'alias gcc="~/research-compiler.sh"' >> ~/.bash_aliases
+  source ~/.bash_aliases
+# else if ( "$SHELL" =~ *tcsh* ) then
+#   # tcsh version (UMD default shell)
+#   curl -k https://raw.github.com/WilDoane/GitDataCollection/master/research-compiler-tcsh.sh -o ~/research-compiler.sh
+#   echo 'alias gcc "~/research-compiler.sh"' >> ~/.aliases
+#   source ~/.aliases
+else
+  echo "this user's shell isn't TCSH or BASH"
+  echo "no setup was performed"
+  exit
+endif
+
+
+# Make the research compiler executable
+chmod 700 ~/research-compiler.sh
+
+cd ~
+if ( -d ~/ENEE150 ) then
+  echo "The user already has an ENEE150 directory..."
+  echo "   using existing directory"
+else
+  mkdir ENEE150  
+endif
+
+cd ENEE150
+
+git init
+touch .gitignore
+printf "\n*.out\n" >> .gitignore
+
+git remote add origin git@github.com:TLPLEngineeringEdResearch/$1.git
+
+cd ~
+if ( -d ~/.ssh ) then
+  echo "The user already has an SSH key..."
+  echo "   check out the ~/.ssh directory and instructions from http://help.github.com/linux-set-up-git/"
+  exit
+else
+  echo "Now you should"
+  echo '  git config --global user.name "Students Name"'
+  echo '  git config --global user.email "student_email@youremail.com"'
+  echo '  ssh-keygen -t rsa -C "student_email@youremail.com"'
+  echo "and accept the default storage directory and enter a blank password"
+  echo "then, "
+  echo "  cat ~/.ssh/id_rsa.pub"
+  echo "and copy and paste it to GitHub's key list"
+endif
+  
