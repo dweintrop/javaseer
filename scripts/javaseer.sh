d6d353b62e800e117e891f551ccc614f114ce50d
dweintrop <dnw882@dweintrop2-SESP.local>
2014-09-08 15:01:23 -0500

    inching closer on getting javaseer posting javac data

--- scripts/javaseer.sh
+++ scripts/javaseer.sh
@@ -14,10 +14,16 @@
 # RESULT=$($GCC $@ 2>&1)
 RESULT=$(javac $@ 2>&1)
 
-DATA = "--data \"student_id=$STUDENT_ID\" --data \"student_name=$STUDENT_NAME\""
-DATA = "$DATA --data java_command=\"$*\" --DATA \"java_program= \" --data \"java_output=$RESULTS\""
+# figure out how to read in java files
+JAVA_PROGRAM="program"
 
-curl --request POST 'http://localhost/Service' "$DATA"
+# post the data to the server
+curl --request POST 'http://localhost:8000/javaseer/' \
+		--data-urlencode "student_id=$STUDENT_ID" \
+		--data-urlencode "student_name=$STUDENT_NAME" \
+		--data-urlencode "javacCall=$*" \
+		--data-urlencode "javaProgram=$JAVA_PROGRAM" \
+		--data-urlencode "javaCompilerOutput=$RESULTS"
 
 # display the compiler output to the user
 echo "$RESULT"
